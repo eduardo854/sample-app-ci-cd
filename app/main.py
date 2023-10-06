cc8b6768193e35c2d7da9c053702d372d689b605
Eduardo Fernandes de Souza <eduardo854@gmail.com>
2023-10-06 16:04:17 -0300

    lint: run lint tool black in folder app

--- app/main.py
+++ app/main.py
@@ -2,10 +2,14 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def status():
     return {"status": "ok"}
 
+
 @app.get("/about")
 def read_root():
-    return {"title": "Building a GitOps structure with GitHub, Actions, DockerHub, and Helm Repository"}
+    return {
+        "title": "Building a GitOps structure with GitHub, Actions, DockerHub, and Helm Repository"
+    }
